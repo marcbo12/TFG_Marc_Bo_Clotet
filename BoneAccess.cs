33b24dc7f8af2178bfa788f429ba3b0ef7a2f48c
marcbo12 <55989383+marcbo12@users.noreply.github.com>
2021-01-23 14:29:12 +0100

    Unity scripts

---
+++ BoneAccess.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoneAccess : MonoBehaviour
+{
+    public Vector3 Offset; 
+    public Transform Target; 
+
+    Animator animator; 
+    Transform head; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        head = GetComponent<Transform>().Find("Anna/hips/spine/chest/chest1/neck/head");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {   
+        if(Target.gameObject.activeSelf){
+            head.LookAt(Target.position);
+            var dir = head.position - this.transform.position; 
+            var angle =  Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+
+        }
+    }
+
+}
